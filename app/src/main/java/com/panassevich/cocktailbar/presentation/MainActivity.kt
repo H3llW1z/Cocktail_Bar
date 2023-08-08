e1a4a5602792fc9aabed755c448cd04e74a48aa5
panassevich <panassevichm@gmail.com>
2023-08-08 14:18:20 +0600

    Initial commit

---
+++ app/src/main/java/com/panassevich/cocktailbar/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.panassevich.cocktailbar.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.panassevich.cocktailbar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
